c8ddb738d0ee3151e0857c82215df9b42bb3e978
AhmedEssam <94251014+ahmedesam2333@users.noreply.github.com>
2026-05-20 21:20:25 +0300

    prepare for deploy on AWS

--- src/DB/connection.db.js
+++ src/DB/connection.db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     const result = await mongoose.connect(process.env.MONGO_URI);
-    console.log(result.models);
     console.log("DB connected successfully 🔥");
   } catch (error) {
     console.log(`fail to connect to DB ⚠️`, error.message);
